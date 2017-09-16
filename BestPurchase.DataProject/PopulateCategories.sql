f676313ad69fc69b36270fca94c8232443b21a60
aspramsh <aspramshadyan@gmail.com>
2017-09-16 22:24:41 +0400

    Cosmetic Changes.

---
+++ BestPurchase.DataProject/PopulateCategories.sql
@@ -0,0 +1,13 @@
+﻿CREATE PROCEDURE [dbo].[PopulateCategories]
+AS
+BEGIN
+	INSERT INTO [dbo].[Category]
+           ([ProductId]
+           ,[ProductCategory])
+     VALUES
+           (1, 'MeatProducts'),
+		   (2, 'FrozenProducts'),
+		   (3, 'Sweets'),
+		   (4, 'FishAndSeaFood'),
+		   (5, 'OrganicProducts')
+END
---
+++ BestPurchase.DataProject/PopulateProductTable.sql
@@ -0,0 +1,15 @@
+﻿CREATE PROCEDURE [dbo].[PopulateProductTable]
+AS
+BEGiN
+	INSERT INTO [dbo].[Product]
+           ([Name]
+           ,[Description]
+           ,[Price]
+           ,[ImageSource])
+     VALUES
+           ('3 Types Of Meat', 'Beef, Pork And Chicken', 3500, 'meats.jpg'),
+		   ('Pizza For Dreamers', '12 Pieces', 2000, 'Frozen.jpg'),
+		   ('Charlie''s Chocolate', '5 types of chocolate with cinnamon flavour', 8000, 'chocolate.jpg'),
+		   ('Gold Fish', '5 types of fresh and tasty fish', 5000, 'seafood.jpg'),
+		   ('Scent of Nature', 'Herbal shampoo for your beauty and health', 4000, 'Shampoo.jpg')
+END
